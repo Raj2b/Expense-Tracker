6c9957e1baa25c04f0311003fc6b3528994a1a75
Raj2b <63523670+Raj2b@users.noreply.github.com>
2025-05-26 19:40:10 -0400

    ui tweak

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -34,7 +34,7 @@ function CardInfo({budgetList}) {
        <div className='p-7 border rounded-lg flex items-center justify-between'>
             <div>
                 <h2 className='text-sm'>Total Budget</h2>
-                <h2 className='font-bold text-2xl'>{totalbudget}</h2>
+                <h2 className='font-bold text-2xl'>${totalbudget}</h2>
             </div>
             <PiggyBank 
             className='bg-primary p-3 h-12 w-12 rounded-full text-white'/>
